a73ae93d0280f47dc53f68977e2b43b0216cb1ed
Vinu Isaac <vinuisaacj@outlook.com>
2014-09-20 10:13:31 +0530

    Create PackDemo.java

---
+++ PackDemo.java
@@ -0,0 +1,33 @@
+package shape;
+
+public class square
+{
+  public void sq(int a)
+  {
+    System.out.println("Are if the square"+(a*a));
+  }
+}
+
+package shape;
+
+
+public class circle
+{
+  public void cir(int r)
+  {
+    System.out.println("Area of the circle="+(3.14*r*r));
+  }
+}
+
+package shape;
+
+public class Triangle
+{
+  public void tri(int w,int h)
+  {
+    System.out.println("Area of the triangle"+(0.5*w*h));
+  }
+}
+
+
+  
