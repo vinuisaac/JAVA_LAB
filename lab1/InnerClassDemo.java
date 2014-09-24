e447e7d4b563d47852805cda8ad385c28de54c72
Vinu Isaac <vinuisaacj@outlook.com>
2014-09-24 11:57:23 +0530

    Update InnerClassDemo.java

--- lab1/InnerClassDemo.java
+++ lab1/InnerClassDemo.java
@@ -1,2 +1,30 @@
 /* Write a Java Program to implement a Inner Class and show its access protection */
 
+class Outer{
+	int out_x=100;
+	void test()
+	{
+		Inner inobj=new Inner();
+		inobj.display();
+	}
+	
+	class Inner{
+		int in_y=10;
+		void display()
+		{
+			System.out.println("Displaying the contents of the outer x:"+out_x);
+		}
+	} 
+	void show()
+	{
+		System.out.println("Displaying the contents of the inner y:"+y );
+	}
+}
+
+class InnerClassDemo{
+	public static void main(String[] args)
+	{
+		Outer outobj=new Outer();
+		outobj.test();
+	}
+}
